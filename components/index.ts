f6176703cbf32e37fa10de6a02a31bb30af2d995
Hugo Curtillet <hcurtillet@eneance.com>
2023-01-29 11:54:45 +0100

    No winner implementation

--- components/index.ts
+++ components/index.ts
@@ -3,3 +3,4 @@ export { Line } from './Line';
 export { Grid } from './Grid';
 export { StackNavigation } from './StackNavigation';
 export * from './ModeSwitcher';
+export * from './WinMessageModal';
